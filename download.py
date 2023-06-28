6d40f9037bd7e00b9b7dc6b67e10512c7e5794ac
Nik <nik@Niks-MacBook-Air.local>
2023-06-28 20:52:37 +0200

    test

---
+++ download.py
@@ -0,0 +1,14 @@
+# This file runs during container build time to get model weights built into the container
+
+# In this example: A Whisper Large model
+from transformers import pipeline
+
+def download_model():
+    # do a dry run of loading the huggingface model, which will download weights
+    pipeline(
+        # 'fill-mask', 
+        model='openai/whisper-large'
+    )
+
+if __name__ == "__main__":
+    download_model()
